db073656d23a19a702f17b8cfa76a0563662b1ce
Matthew Sumner <msumner@hubspot.com>
2023-12-01 15:19:25 -0500

    Day One; Part One

--- src/DayOne.tsx
+++ src/DayOne.tsx
@@ -1,3 +1,27 @@
+import { input } from './DayOne/input';
+
+const result = input.split("\n").map((string: String) => {
+  const numbers = string.match(/(\d)/g)
+
+  if (!numbers) {
+    throw new Error('there were no numbers!');
+  }
+
+  const firstNumber = parseInt(numbers[0])
+  const lastNumber = parseInt(numbers[numbers.length - 1])
+
+  if (numbers.length === 1) {
+    return firstNumber * 10 + firstNumber;
+  } else {
+    return firstNumber * 10 + lastNumber;
+  }
+})
+
 export const DayOne = () => {
-  return <h1>HI</h1>;
+  return (
+    <div>
+      <h1>Part One</h1>
+      <div>{result.join(' + ')} = {result.reduce((a, b) => a + b)}</div>
+    </div>
+  )
 };
